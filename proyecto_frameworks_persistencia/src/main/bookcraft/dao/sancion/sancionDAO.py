bf8920e22fef8584484932365e0513322c859eea
LeonardoRHernandez <leonardoroque711@gmail.com>
2024-05-28 16:39:52 -0600

    modificando DAO sancion

--- proyecto_frameworks_persistencia/src/main/bookcraft/dao/sancion/sancionDAO.py
+++ proyecto_frameworks_persistencia/src/main/bookcraft/dao/sancion/sancionDAO.py
@@ -1,4 +1,7 @@
+from datetime import datetime, timedelta
+from ...bd.domain.sancion import Sancion
 from ...bd.mappers.sancion.sancion_map import SancionMapper
+
 class SancionDAO:
     def __init__(self):
         self.__sancion = None
@@ -11,3 +14,14 @@ class SancionDAO:
     def get_sancion(self):
         return self.__sancion
     
+    def crear_sancion(self, descripcion):
+        
+        fecha_inicio = datetime.now().date()
+        fecha_fin = fecha_inicio + timedelta(days=3)
+        sancion = Sancion(fecha_inicio=fecha_inicio, fecha_fin=fecha_fin, descripcion=descripcion)
+
+        sancion_dao = SancionDAO()
+        id_sancion = sancion_dao.set_sancion(sancion)
+            
+        return id_sancion
+
